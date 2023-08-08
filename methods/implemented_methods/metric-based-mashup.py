1fcf46271c0bd5d13920dd3beb9b0a482ef18e26
Michal Spiegel <spiegel.michal@gmail.com>
2023-08-08 18:37:11 +0200

    feat: implement untuned new method prototype

--- methods/implemented_methods/metric-based-mashup.py
+++ methods/implemented_methods/metric-based-mashup.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -11,7 +12,7 @@ from tensorflow.keras.losses import BinaryCrossentropy
 from sklearn.model_selection import train_test_split
 from methods.utils import timeit, get_clf_results
 from methods.abstract_methods.experiment import Experiment
-from methods.utils import get_rank, get_ll, get_entropy
+from methods.utils import get_rank, get_ll, get_entropy, cal_metrics
 
 class MetricBasedMashup(Experiment):
     
@@ -23,10 +24,10 @@ class MetricBasedMashup(Experiment):
         self.DEVICE = DEVICE
     
     def criterion_fn(self, text: str):
-        return (get_rank(text, self.model, self.tokenizer, self.DEVICE),
+        return np.array([get_rank(text, self.model, self.tokenizer, self.DEVICE),
                 get_rank(text, self.model, self.tokenizer, self.DEVICE, log=True),
                 get_ll(text, self.model, self.tokenizer, self.DEVICE),
-                get_entropy(text, self.model, self.tokenizer, self.DEVICE))
+                get_entropy(text, self.model, self.tokenizer, self.DEVICE)])
 
     @timeit
     def run(self):
@@ -37,39 +38,37 @@ class MetricBasedMashup(Experiment):
         train_text = self.data['train']['text']
         train_label = self.data['train']['label']
         t1 = time.time()
-        train_criterion = [self.criterion_fn(train_text[idx])
-                        for idx in range(len(train_text))]
+        train_criterion = [self.criterion_fn(train_text[idx]) 
+                           for idx in tqdm(range(len(train_text)), desc="Computing metrics on train partition")]
         x_train = np.array(train_criterion)
-        #x_train = np.expand_dims(x_train, axis=-1)
         y_train = np.expand_dims(train_label, axis=-1)
 
         test_text = self.data['test']['text']
         test_label = self.data['test']['label']
         test_criterion = [self.criterion_fn(test_text[idx])
-                        for idx in range(len(test_text))]
+                        for idx in tqdm(range(len(test_text)), desc="Computing test metrics on test partition")]
         x_test = np.array(test_criterion)
-        #x_test = np.expand_dims(x_test, axis=-1)
         y_test = np.expand_dims(test_label, axis=-1)
-
-        x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
         
-        # Train and test FFNN 
-        #train_pred, test_pred, train_pred_prob, test_pred_prob, train_res, test_res = get_clf_results(x_train, y_train, x_test, y_test)
         model = build_FFNN()
-        print(x_train.shape, y_train.shape, x_test.shape, y_test.shape, x_val.shape, y_val.shape)
-        train_res = model.fit(np.array(x_train), np.array(y_train).astype(np.float32), epochs=50, batch_size=25, validation_data=(np.array(x_val), np.array(y_val).astype(np.float32)), shuffle=True)
-        test_res = model.evaluate(np.array(x_test), np.array(y_test).astype(np.float32), batch_size=25)
-        print(train_res)
-        print(test_res)
-        exit(0)
-        acc_train, precision_train, recall_train, f1_train, auc_train = train_res["Accuracy"], train_res["Precision"], train_res["Recall"], train_res["F1"]
-        acc_test, precision_test, recall_test, f1_test, auc_test = test_res
+        num_epochs = 30
+        batch_size = 32
+        train_history = model.fit(x_train, y_train.astype(np.float32), epochs=num_epochs, batch_size=batch_size, validation_split=0.2, shuffle=True)
+        training_phase_data = process_training_phase_data(train_history, num_epochs=num_epochs)
+
+        train_pred_prob = model.predict(x_train, batch_size=batch_size).reshape((-1,))
+        test_pred_prob = model.predict(x_test, batch_size=batch_size).reshape((-1,))
+        train_pred = np.array([1 if prob >= 0.5 else 0 for prob in train_pred_prob])
+        test_pred = np.array([1 if prob >= 0.5 else 0 for prob in test_pred_prob])
+        
+        acc_train, precision_train, recall_train, f1_train, auc_train = cal_metrics(train_label, train_pred, train_pred_prob)
+        acc_test, precision_test, recall_test, f1_test, auc_test = cal_metrics(test_label, test_pred, test_pred_prob)
 
         print(f"{self.name} acc_train: {acc_train}, precision_train: {precision_train}, recall_train: {recall_train}, f1_train: {f1_train}, auc_train: {auc_train}")
         print(f"{self.name} acc_test: {acc_test}, precision_test: {precision_test}, recall_test: {recall_test}, f1_test: {f1_test}, auc_test: {auc_test}")
 
         return {
-            'name': f'{self.name}_threshold',
+            'name': f'{self.name}',
             'input_data': self.data,
             'predictions': {'train': train_pred, 'test': test_pred},
             'machine_prob': {'train': train_pred_prob, 'test': test_pred_prob},
@@ -86,33 +85,37 @@ class MetricBasedMashup(Experiment):
                     'recall': recall_test,
                     'f1': f1_test
                 }
-            }
+            },
+            'training_phase_data': training_phase_data
         }
 
 def build_FFNN():
     model = Sequential()
 
-    model.add(Dense(16, kernel_initializer='normal',input_dim = 4, activation='tanh'))
-    model.add(Dense(8, kernel_initializer='normal',activation='relu'))
-    model.add(Dense(2, kernel_initializer='normal',activation='relu'))
+    model.add(Dense(128, kernel_initializer='normal', input_dim=4, activation='relu'))
+    model.add(Dense(64, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(32, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal',activation='sigmoid'))
    
     model.compile(loss=BinaryCrossentropy(), optimizer='adam', metrics=[F1Score(), Accuracy(), Precision(), Recall(), AUC()])
     model.summary()
     return model
     
 
-def visualize():
-    import hiplot as hip
-
-    num_epochs = 50
-    data = [{'epoch': idx,
-        'loss': history.history['loss'][idx],
-        'RMSE': history.history['root_mean_squared_error'][idx],
-        'MAE': history.history['mean_absolute_error'][idx],
-        'val_loss': history.history['val_loss'][idx],
-        'val_RMSE': history.history['val_root_mean_squared_error'][idx],
-        'val_MAE': history.history['val_mean_absolute_error'][idx],
+def process_training_phase_data(history, num_epochs):
+    data = [{'epoch': str(idx),
+        'loss': str(history.history['loss'][idx]),
+        'f1_score': str(history.history['f1_score'][idx][0]),
+        'accuracy': str(history.history['accuracy'][idx]),
+        'precision': str(history.history['precision'][idx]),
+        'recall': str(history.history['recall'][idx]),
+        'auc': str(history.history['auc'][idx]),
+        'val_loss': str(history.history['val_loss'][idx]),
+        'val_f1_score': str(history.history['val_f1_score'][idx][0]),
+        'val_accuracy': str(history.history['val_accuracy'][idx]),
+        'val_precision': str(history.history['val_precision'][idx]),
+        'val_recall': str(history.history['val_recall'][idx]),
+        'val_auc': str(history.history['val_auc'][idx])
     } 
         for idx in range(num_epochs)]
-
-    hip.Experiment.from_iterable(data).display()
+    return data
