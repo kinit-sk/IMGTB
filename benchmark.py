393776695cb13bf8457db6327bfc428171b09383
Michal Spiegel <spiegel.michal@gmail.com>
2023-08-09 16:41:10 +0200

    feat: add help messages, prettify stdout output, add experiment failure handling

--- benchmark.py
+++ benchmark.py
@@ -6,7 +6,7 @@ import json
 from pathlib import Path
 from inspect import getmembers, getmodule, isclass
 import importlib.util
-import pickle as pkl
+import traceback
 
 import dataset_loader
 from methods.utils import load_base_model, load_base_model_and_tokenizer, filter_test_data
@@ -18,6 +18,7 @@ METHODS_DIRECTORY = "methods/implemented_methods"
 RESULTS_PATH = "results"
 LOG_PATH = os.path.join(RESULTS_PATH, "logs", CURR_DATETIME)
 LOG_METHOD_W_DATASET_PATH = os.path.join(RESULTS_PATH, "methods")
+SEP_LINE = "-----------------------------------------------------"
 
 
 def scan_for_detection_methods():
@@ -43,8 +44,7 @@ def scan_for_detection_methods():
 def log_whole_experiment(args, outputs):
     """Log all experiment data as a whole by current time"""
         
-    print(f"Saving results to absolute path: {os.path.abspath(LOG_PATH)}")
-    print(LOG_PATH)
+    print(f"Logging all experiment data to absolute path: {os.path.abspath(LOG_PATH)}")
     if not os.path.exists(LOG_PATH):
         os.makedirs(LOG_PATH)
     
@@ -53,8 +53,8 @@ def log_whole_experiment(args, outputs):
         json.dump(args.__dict__, file, indent=4)
         
     # Save outputs.
-    with open(os.path.join(LOG_PATH, f"benchmark_results.pkl"), "wb") as file:
-        pkl.dump(outputs, file)
+    with open(os.path.join(LOG_PATH, f"benchmark_results.json"), "w") as file:
+        json.dump(outputs, file)
 
 def save_method_dataset_combination_results(args, outputs):
     """Log results for all method and dataset combinations separately"""
@@ -69,45 +69,61 @@ def save_method_dataset_combination_results(args, outputs):
                 os.makedirs(SAVE_PATH)
             data = {"args": args.__dict__, "data": method_data}
             # Save args and outputs for given method and dataset.
-            with open(os.path.join(SAVE_PATH, f"{CURR_DATETIME}_experiment_results.pkl"), "wb") as file:
-                pkl.dump(outputs, file)
-            
+            with open(os.path.join(SAVE_PATH, f"{CURR_DATETIME}_experiment_results.json"), "w") as file:
+                json.dump(data, file)
+
+def print_w_sep_line(text: str) -> None:
+    width = os.get_terminal_size().columns 
+    print('-' * width)
+    print(text)
+               
     
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     
     # dataset params
-    parser.add_argument('--dataset_filepaths', nargs='+', type=str, default=["datasets/TruthfulQA_LMMs.csv"])
-    parser.add_argument('--dataset_processors', nargs='+', type=str, default=["default"])
-    parser.add_argument('--text_field', type=str, default="text")
-    parser.add_argument('--label_field', type=str, default="label")
-    parser.add_argument('--human_label', type=str, default="0")
-    parser.add_argument('--detectLLM', type=str, default="ChatGPT")
+    parser.add_argument('--dataset_filepath', nargs='+', type=str, default=["datasets/TruthfulQA_LMMs.csv"], 
+                        help="List of datasets (filepaths)."
+                             "(For datasets split across multiple files, specify only the directory where the files are all located.)")
+    parser.add_argument('--dataset_processor', nargs='+', type=str, default=["default"], 
+                        help="List of custom dataset processing functions corresponding to the dataset filepaths positionally.")
+    parser.add_argument('--text_field', type=str, default="text", 
+                        help="Name of the dataset column containing text.")
+    parser.add_argument('--label_field', type=str, default="label", 
+                        help="Name of the dataset column containing labels.")
+    parser.add_argument('--human_label', type=str, default="0", 
+                        help="String corresponding to the label marking human text.")
+    parser.add_argument('--detectLLM', type=str, default="ChatGPT", 
+                        help="For supported datasets, datasets with texts from multiple LLMs, "
+                             "you can pass to the processor function against which LLM you want to run the benchmark.")
 
     # Use dataset_other to pass arbitrary text information from CLI to chosen dataset processor
-    parser.add_argument('--dataset_other', nargs="+", type=str)
+    parser.add_argument('--dataset_other', nargs="+", type=str, 
+                        help="Use to pass arbitrary text information to chosen dataset processor.")
     
     # List the methods you want to run
     # (methods are named after names of their respective classes in the methods/implemented_methods directory)
-    parser.add_argument('--methods', nargs='+', type=str, default=["all"])
-    parser.add_argument('--list_methods', action="store_true")
+    parser.add_argument('--methods', nargs='+', type=str, default=["all"], help="List the names of methods you want to run.")
+    parser.add_argument('--list_methods', action="store_true", help="List names of all available methods.")
     # Select an algorithm that will be used for threshold computation (for metric-based methods)
     # (You can define your own in methods/utils.py source file by creating a new item in the CLF_MODELS dictionary)
-    parser.add_argument('--clf_algo_for_threshold', type=str, default="LogisticRegression", choices=["LogisticRegression", 
-                                                                                                     "KNeighborsClassifier", 
-                                                                                                     "SVC", 
-                                                                                                     "DecisionTreeClassifier", 
-                                                                                                     "RandomForestClassifier", 
-                                                                                                     "MLPClassifier", 
-                                                                                                     "AdaBoostClassifier"])
+    parser.add_argument('--clf_algo_for_threshold', type=str, default="LogisticRegression", 
+                        choices=["LogisticRegression", 
+                                 "KNeighborsClassifier", 
+                                 "SVC", 
+                                 "DecisionTreeClassifier", 
+                                 "RandomForestClassifier", 
+                                 "MLPClassifier", 
+                                 "AdaBoostClassifier"],
+                        help="Specify a classification algorithm to be used for threshold computation.")
     
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--base_model_name', type=str, default="gpt2-medium")
     parser.add_argument('--mask_filling_model_name',
                         type=str, default="t5-large")
     parser.add_argument('--cache_dir', type=str, default=".cache")
-    parser.add_argument('--DEVICE', type=str, default="cuda")
+    parser.add_argument('--DEVICE', type=str, default="cuda", help="Define a device to run the computations on (e.g. cuda, cpu...).")
 
     # params for DetectGPT
     parser.add_argument('--pct_words_masked', type=float, default=0.3)
@@ -141,8 +157,8 @@ if __name__ == '__main__':
     START_DATE = datetime.datetime.now().strftime('%Y-%m-%d')
     START_TIME = datetime.datetime.now().strftime('%H-%M-%S-%f')
     
-    print(f'Loading datasets {args.dataset_filepaths}...')
-    dataset_dict = dataset_loader.load_multiple_from_file(filepaths=args.dataset_filepaths, processors=args.dataset_processors, 
+    print_w_sep_line(f'Loading datasets {args.dataset_filepaths}...')
+    dataset_dict = dataset_loader.load_multiple_from_file(filepaths=args.dataset_filepath, processors=args.dataset_processor, 
                                                           text_field=args.text_field, label_field=args.label_field, 
                                                           human_label=args.human_label, other=args.dataset_other)
     # data = filter_test_data(data, max_length=25)
@@ -156,9 +172,10 @@ if __name__ == '__main__':
     cache_dir = args.cache_dir
     if not os.path.exists(cache_dir):
         os.makedirs(cache_dir)
-    print(f"Using cache dir {cache_dir}")
+    print_w_sep_line(f"Using cache dir {cache_dir}")
 
-    # get generative model
+    # Get generative model
+    print_w_sep_line(f"Loading BASE model {args.base_model_name}\n")
     base_model, base_tokenizer = load_base_model_and_tokenizer(
         args.base_model_name, cache_dir)
     load_base_model(base_model, DEVICE)
@@ -166,32 +183,42 @@ if __name__ == '__main__':
     # Load experiments and evaluate them
     experiments = scan_for_detection_methods()
     if args.list_methods:
-        print("\nAvailable methods:\n")
+        print_w_sep_line("Available methods:\n")
         for exp in experiments: print(exp.__name__)
-        print("\nFinish")
+        print_w_sep_line("Finish")
         exit(0)
     
     outputs = dict()
     for dataset_name, data in dataset_dict.items():
-        print(f"Running experiments on {dataset_name} dataset:")    
+        print_w_sep_line(f"Running experiments on {dataset_name} dataset:\n")    
         filtered = filter(lambda exp: args.methods[0] == "all" or exp.__name__ in args.methods, experiments)
-        outputs[dataset_name] = list(map(lambda obj: obj(data=data, 
-                                            model=base_model, 
-                                            tokenizer=base_tokenizer, 
-                                            DEVICE=DEVICE, 
-                                            detectLLM=args.detectLLM, 
-                                            batch_size=batch_size,
-                                            cache_dir=cache_dir,
-                                            args=args,
-                                            gptzero_key=args.gptzero_key,
-                                            clf_algo_for_threshold=args.clf_algo_for_threshold
-                                            ).run(), filtered))
+        outputs[dataset_name] = []
+        for experiment in filtered:
+            try: 
+                results = experiment(data=data, 
+                                        model=base_model, 
+                                        tokenizer=base_tokenizer, 
+                                        DEVICE=DEVICE, 
+                                        detectLLM=args.detectLLM, 
+                                        batch_size=batch_size,
+                                        cache_dir=cache_dir,
+                                        args=args,
+                                        gptzero_key=args.gptzero_key,
+                                        clf_algo_for_threshold=args.clf_algo_for_threshold
+                                        ).run()
+            except Exception:
+                print(f"Experiment {experiment.__name__} failed due to below reasons. Skipping and continuing with the next experiment.")
+                print(traceback.format_exc())
+                continue
+            
+            outputs[dataset_name].append(results)
 
+    print_w_sep_line("Saving experiment results\n")
     log_whole_experiment(args, outputs)    
     save_method_dataset_combination_results(args, outputs)
     
-    print("\nRunning analysis:")
+    print_w_sep_line("Running analysis:\n")
     run_full_analysis(outputs, save_path=LOG_PATH)
     
-    print("Finish")
+    print_w_sep_line("Finish")
     
