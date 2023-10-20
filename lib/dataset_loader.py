ac8d8bb9880667e348d8835eecc4e992139ebabd
Michal Spiegel <spiegel.michal@gmail.com>
2023-10-20 17:40:52 +0200

    fix: small bugs in data loading/testing

--- lib/dataset_loader.py
+++ lib/dataset_loader.py
@@ -29,6 +29,7 @@ SUPPORTED_FILETYPES = ["auto", "csv", "tsv", "xls", "xlsx", "json", "jsonl", "xm
 def load_multiple_from_file(datasets_params, is_interactive: bool):
     unified_data_dict = dict()
     dataset_dict = read_multiple_to_pandas(datasets_params, is_interactive)
+
     for dataset_data, dataset_params in zip(dataset_dict.items(), datasets_params):
         dataset_name, file_dict = dataset_data
         dataset_processor = globals().get(f'process_{dataset_params["processor"]}')
@@ -41,24 +42,27 @@ def load_multiple_from_file(datasets_params, is_interactive: bool):
 
 def read_multiple_to_pandas(datasets_params, is_interactive: bool) -> Dict[str, pd.DataFrame]:
     datasets = dict()
+    seen = dict()
     for dataset_params in datasets_params:
         filepath, filetype = dataset_params["filepath"], dataset_params["filetype"]
         read_dataset_to_pandas = read_dir_to_pandas if Path(filepath).is_dir() else read_file_to_pandas
-        while (df_dict := read_dataset_to_pandas(filepath, filetype, is_interactive)) is None:
+        while (df_dict := read_dataset_to_pandas(filepath=filepath, filetype=filetype, is_interactive=is_interactive, config=dataset_params)) is None:
             if is_interactive:
                 filetype = input(f'Unknown dataset file format for: {filepath}. Please, input the correct file format manually.\n'
                                 f'Options: {", ".join(SUPPORTED_FILETYPES)}\n')
                 continue
             raise ValueError(f'Unknown dataset file format for: {filepath}')
-        datasets[Path(filepath).stem] = df_dict
+        
+        name = Path(filepath).stem if filetype != "huggingface" else filepath
+        datasets[_get_unique_name(name, seen)] = df_dict
     
     return datasets
 
-def read_dir_to_pandas(filepath: str, filetype: str, is_interactive: bool):
+def read_dir_to_pandas(filepath: str, filetype: str, is_interactive: bool, config):
     data = dict()
     pathlist = Path(filepath).iterdir()
     for path in pathlist:
-        while (df_dict := read_file_to_pandas(str(path), filetype)) is None:
+        while (df_dict := read_file_to_pandas(str(path), config, filetype=filetype, is_interactive=is_interactive)) is None:
             if is_interactive:
                 filetype = input(f'Unknown dataset file format for: {filepath}. Please, input the correct file format manually.\n'
                                 f'Options: {", ".join(SUPPORTED_FILETYPES)}\n')
@@ -69,7 +73,7 @@ def read_dir_to_pandas(filepath: str, filetype: str, is_interactive: bool):
     return data
     
 
-def read_file_to_pandas(filepath: str, filetype="auto", *args):
+def read_file_to_pandas(filepath: str, config, is_interactive: bool, filetype="auto"):
     data = dict()
     if filetype == "auto":
         filetype = filepath.rsplit(".", 1)[-1]
@@ -88,10 +92,10 @@ def read_file_to_pandas(filepath: str, filetype="auto", *args):
         case "xml":
             data[Path(filepath).stem] = pd.read_xml(filepath)
         case "huggingfacehub":
-            data[Path(filepath).stem] = datasets.load_dataset(filepath)
+            data[Path(filepath).stem] = datasets.load_dataset(filepath, config["configuration"])
         case _:
             return None
-    
+
     return data 
 
 #######################################################
@@ -101,10 +105,12 @@ def read_file_to_pandas(filepath: str, filetype="auto", *args):
 def process_default(data: Dict[str, pd.DataFrame], config) -> pd.DataFrame:
     if config["filetype"] == "huggingfacehub":
         return process_huggingfacehub(data, config)
-
+    
+    dataset_name = list(data.keys())[0]
     data = list(data.values())[0]    
+    
     if config["text_field"] not in data.columns or config["label_field"] not in data.columns:
-        raise ValueError("Could not parse dataset."
+        raise ValueError(f"Could not parse dataset {dataset_name}."
                          "Text and label fields are not specified correctly."
                          "Please, correctly specify dataset specifications or" 
                          "define your own custom function for parsing.")
@@ -124,48 +130,60 @@ def process_default(data: Dict[str, pd.DataFrame], config) -> pd.DataFrame:
     return {"train": data_train.reset_index().to_dict(orient='list'), "test": data_test.reset_index().to_dict(orient='list')}
 
 
-def add_labels_to_text_only_huggingface(dataset: Union[datasets.Dataset, datasets.DatasetDict], 
-                                        splits: List[str], 
-                                        label: str) -> Union[datasets.Dataset, datasets.DatasetDict]:
-    if isinstance(dataset, datasets.Dataset):
-        dataset.add_columns("label", [label]*dataset.num_rows)
-    elif isinstance(dataset, datasets.DatasetDict):
-        for split in splits:
-            if split is not None:
-                dataset[split].add_columns("label", [label]*dataset[split].num_rows)
-    return dataset
-
 def process_huggingfacehub(data, config):
     HUMAN_LABEL = 0
     MACHINE_LABEL = 1
+    dataset_name = list(data.keys())[0]
     data = list(data.values())[0]
-    if (config["train_split"] is not None and \
-        config["train_split"] not in data.column_names.keys()) or \
-       config["test_split"] not in data.column_names.keys() or \
-       config["text_field"] not in data.column_names[config["train_split"]] or \
-       config["label_field"] not in data.column_names[config["train_split"]] or \
-       config["text_field"] not in data.column_names[config["test_split"]] or \
-       config["label_field"] not in data.column_names[config["test_split"]]:
-        raise ValueError("Could not parse dataset."
-                         "Splits, text or label fields are not specified correctly."
-                         "Please, correctly specify dataset specifications or" 
-                         "define your own custom function for parsing.")
+
+    if isinstance(data, datasets.DatasetDict) and len(data.column_names.keys()) == 1:
+        data = data[list(data.column_names.keys())[0]]
     
+    if isinstance(data, datasets.DatasetDict) and \
+       ((config["train_split"] is not None and \
+         config["train_split"] not in data.column_names.keys()) or \
+        config["test_split"] not in data.column_names.keys()):
+           raise ValueError(f"Could not parse dataset. Incorrectly specified splits for {dataset_name} dataset.")
+    
+    if isinstance(data, datasets.DatasetDict) and \
+       (config["train_split"] is not None and \
+        ((config["text_field"] not in data.column_names[config["train_split"]] or \
+          (config["label_field"] not in data.column_names[config["train_split"]] and \
+           config["processor"] != "human_only" and \
+           config["processor"] != "machine_only")))):
+           raise ValueError(f"Could not parse dataset. Incorrectly specified text or label fields for {dataset_name} dataset in train split.")
+
+    if isinstance(data, datasets.DatasetDict) and \
+        (config["text_field"] not in data.column_names[config["test_split"]] or \
+        (config["label_field"] not in data.column_names[config["test_split"]] and \
+         config["processor"] != "human_only" and \
+         config["processor"] != "machine_only")):
+           raise ValueError(f"Could not parse dataset. Incorrectly specified text or label fields for {dataset_name} dataset in test split.")
+
+    if isinstance(data, datasets.Dataset) and \
+       (config["text_field"] not in data.column_names or \
+        (config["label_field"] not in data.column_names and \
+         config["processor"] != "human_only" and \
+         config["processor"] != "machine_only")):
+           raise ValueError(f"Could not parse dataset. Incorrectly specified text or label fields for {dataset_name} dataset.")
+        
     if config["processor"] == "human_only":
-        data = add_labels_to_text_only_huggingface(data, [config["train_split"], config["test_split"]], HUMAN_LABEL)
+        data = _add_labels_to_text_only_huggingface(data, [config["train_split"], config["test_split"]], HUMAN_LABEL)
     if config["processor"] == "machine_only":
-        data = add_labels_to_text_only_huggingface(data, [config["train_split"], config["test_split"]], MACHINE_LABEL)
+        data = _add_labels_to_text_only_huggingface(data, [config["train_split"], config["test_split"]], MACHINE_LABEL)
     
     if config["test_size"] == 1 or config["train_split"] is None:
         return {"train": {"text": [], "label": []}, "test": data[config["test_split"]].to_dict()}
     
     if isinstance(data, datasets.Dataset):
+        data = data.to_pandas().loc[:, [config["text_field"], config["label_field"]]]
+        data[config["label_field"]] = data[config["label_field"]].astype(str) != config["human_label"]
         # Try to stratify, if possible
         try:
-            data_train, data_test = train_test_split(data.to_pandas(), test_size=config["test_size"], random_state=42, shuffle=config["shuffle"], stratify=data["label"])
+            data_train, data_test = train_test_split(data, test_size=config["test_size"], random_state=42, shuffle=config["shuffle"], stratify=data["label"])
         except ValueError:
             print(f"Could not stratify train-test split for dataset {config['filepath']}. Continuing without...")
-            data_train, data_test = train_test_split(data.to_pandas(), test_size=config["test_size"], random_state=42, shuffle=config["shuffle"], stratify=None)
+            data_train, data_test = train_test_split(data, test_size=config["test_size"], random_state=42, shuffle=config["shuffle"], stratify=None)
         
         return {"train": data_train.reset_index().to_dict(orient='list'), "test": data_test.reset_index().to_dict(orient='list')}
 
@@ -175,16 +193,21 @@ def process_huggingfacehub(data, config):
            
 
 def process_human_only(data, config):
-    HUMAN_LABEL = 0
     if config["filetype"] == "huggingfacehub":
         return process_huggingfacehub(data, config)
+    
+    HUMAN_LABEL = 0
+    dataset_name = list(data.keys())[0]
+    data = list(data.values())[0]
+    
+    if len(data.columns) == 1:
+        data.columns = [config["text_field"]]
     if config["text_field"] not in data.columns:
-        raise ValueError("Could not parse dataset."
+        raise ValueError(f"Could not parse dataset {dataset_name}."
                          "Text field is not specified correctly."
                          "Please, correctly specify dataset specifications or" 
                          "define your own custom function for parsing.")
     
-    data = list(data.values())[0]
     data = data.loc[:, [config["text_field"]]]
     data[config["label_field"]] = pd.Series([HUMAN_LABEL]*data[config["text_field"]].size)
     
@@ -202,16 +225,21 @@ def process_human_only(data, config):
     
 
 def process_machine_only(data, config):
-    MACHINE_LABEL = 1
     if config["filetype"] == "huggingfacehub":
         return process_huggingfacehub(data, config)
+    
+    MACHINE_LABEL = 1
+    dataset_name = list(data.keys())[0]
+    data = list(data.values())[0]
+    
+    if len(data.columns) == 1:
+        data.columns = [config["text_field"]]
     if config["text_field"] not in data.columns:
-        raise ValueError("Could not parse dataset."
+        raise ValueError(f"Could not parse dataset {dataset_name}."
                          "Text field is not specified correctly."
                          "Please, correctly specify dataset specifications or" 
                          "define your own custom function for parsing.")
     
-    data = list(data.values())[0]
     data = data.loc[:, [config["text_field"]]]
     data[config["label_field"]] = pd.Series([MACHINE_LABEL]*data[config["text_field"]].size)
     
@@ -386,3 +414,22 @@ def _process_text_truthfulqa_adv(text):
         start_idx = first_period + 2
         text = text[start_idx:]
     return text
+
+def _get_unique_name(name: str, seen: Dict[str, int]) -> str:
+    if name not in seen:
+        seen[name] = 1
+        return name
+    else:
+        seen[name] += 1
+        return name + str(seen[name])
+    
+def _add_labels_to_text_only_huggingface(dataset: Union[datasets.Dataset, datasets.DatasetDict], 
+                                        splits: List[str], 
+                                        label: str) -> Union[datasets.Dataset, datasets.DatasetDict]:
+    if isinstance(dataset, datasets.Dataset):
+        dataset = dataset.add_column("label", [label]*dataset.num_rows)
+    elif isinstance(dataset, datasets.DatasetDict):
+        for split in splits:
+            if split is not None:
+                dataset[split] = dataset[split].add_column("label", [label]*dataset[split].num_rows)
+    return dataset
--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,7 +2,7 @@ import os
 
 if __name__ == "__main__":
     os.system("python benchmark.py --from_config tests/testing_config.yaml")
-    os.system("python benchmark.py -i --name only_test_to_be_removed --dataset tests/datasets/test_mix_labels.csv --dataset tests/datasets/test_machine_only --methods all EntropyMetric DetectGPT --clf_algo_for_threshold RandomForestClassifier --base_model_name prajjwal1/bert-tiny --mask_filling_model_name google/t5-efficient-tiny --analysis_methods all")
+    os.system("python benchmark.py -i --name only_test_to_be_removed --dataset tests/datasets/test_mix_labels.csv --dataset tests/datasets/test_machine_only. --methods all EntropyMetric DetectGPT --clf_algo_for_threshold RandomForestClassifier --base_model_name prajjwal1/bert-tiny --mask_filling_model_name google/t5-efficient-tiny --analysis_methods all")
     os.system("python benchmark.py --list_datasets")
     os.system("python benchmark.py --list_methods")
     os.system("python benchmark.py --list_analysis_methods")
