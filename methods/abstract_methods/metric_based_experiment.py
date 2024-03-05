5612b3c4c8a0dc058826859547f0a737a9ca2e7d
michalspiegel <spiegel.michal@gmail.com>
2024-03-05 15:11:10 +0100

    dev: extract metric-based methods specific code from utils

--- methods/abstract_methods/metric_based_experiment.py
+++ methods/abstract_methods/metric_based_experiment.py
@@ -4,9 +4,28 @@ import os
 import torch
 import torch.nn.functional as F
 import time
-from methods.utils import timeit, get_clf_results, load_base_model_and_tokenizer, move_model_to_device
+from methods.utils import timeit, move_model_to_device, cal_metrics
 from methods.abstract_methods.experiment import Experiment
 import gc
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.neural_network import MLPClassifier
+
+
+CLF_MODELS = {
+    "LogisticRegression": LogisticRegression,
+    "KNeighborsClassifier": KNeighborsClassifier,
+    "SVC": SVC,
+    "DecisionTreeClassifier": DecisionTreeClassifier,
+    "RandomForestClassifier": RandomForestClassifier,
+    "MLPClassifier": MLPClassifier,
+    "AdaBoostClassifier": AdaBoostClassifier
+}
+
 
 class MetricBasedExperiment(Experiment):
     
@@ -17,6 +36,7 @@ class MetricBasedExperiment(Experiment):
         self.DEVICE = config["DEVICE"]
         self.base_model = None
         self.base_tokenizer = None
+        self.threshold = config.get("threshold")
         self.config = config
     
     def criterion_fn(self, text: str):
@@ -97,7 +117,7 @@ class MetricBasedExperiment(Experiment):
                     'recall': recall_train,
                     'f1': f1_train
                 },
-                'test': {
+                'test': {mator based on name
                     'acc': acc_test,
                     'precision': precision_test,
                     'recall': recall_test,
@@ -106,3 +126,51 @@ class MetricBasedExperiment(Experiment):
             },
             "config": self.config
         }
+
+
+def threshold_calibration():
+    pass
+
+def get_clf_results(x_train, y_train, x_test, y_test, config):
+
+    clf_algo_config = config["clf_algo_for_threshold"]
+    clf_algo_name = clf_algo_config["name"]
+    if clf_algo_name is None:
+        
+    if CLF_MODELS.get(clf_algo_name) is None:
+        raise ValueError(f"Unsupported classification algorithm for threshold computation selected: {clf_algo_name}")
+    
+    clf_algo = CLF_MODELS[clf_algo_name]
+    clf_model = clf_algo(**{key: value for key, value in clf_algo_config.items() if key != 'name'})
+    clf = clf_model.fit(x_train, y_train)
+
+    y_train_pred = clf.predict(x_train)
+    y_train_pred_prob = clf.predict_proba(x_train)
+    y_train_pred_prob = [_[1] for _ in y_train_pred_prob]
+    acc_train, precision_train, recall_train, f1_train, auc_train = cal_metrics(
+        y_train, y_train_pred, y_train_pred_prob)
+    train_res = acc_train, precision_train, recall_train, f1_train, auc_train
+
+    y_test_pred = clf.predict(x_test)
+    y_test_pred_prob = clf.predict_proba(x_test)
+    
+    y_test_pred_prob = [_[1] for _ in y_test_pred_prob]
+    acc_test, precision_test, recall_test, f1_test, auc_test = cal_metrics(
+        y_test, y_test_pred, y_test_pred_prob)
+    test_res = acc_test, precision_test, recall_test, f1_test, auc_test
+
+    return y_train_pred, y_test_pred, y_train_pred_prob, y_test_pred_prob, train_res, test_res
+
+def load_base_model_and_tokenizer(name, cache_dir):
+
+    base_model = transformers.AutoModelForCausalLM.from_pretrained(
+        name, cache_dir=cache_dir)
+    base_tokenizer = transformers.AutoTokenizer.from_pretrained(
+        name, cache_dir=cache_dir)
+    base_tokenizer.pad_token_id = base_tokenizer.eos_token_id
+    if base_tokenizer.pad_token is None:
+        base_tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        base_model.resize_token_embeddings(len(base_tokenizer))
+
+
+    return base_model, base_tokenizer
--- methods/utils.py
+++ methods/utils.py
@@ -2,28 +2,12 @@ import transformers
 import re
 import sys
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.neural_network import MLPClassifier
 import time
 from functools import wraps
 import random
 import torch
 import torch.nn.functional as F
 import traceback
-
-CLF_MODELS = {
-    "LogisticRegression": LogisticRegression,
-    "KNeighborsClassifier": KNeighborsClassifier,
-    "SVC": SVC,
-    "DecisionTreeClassifier": DecisionTreeClassifier,
-    "RandomForestClassifier": RandomForestClassifier,
-    "MLPClassifier": MLPClassifier,
-    "AdaBoostClassifier": AdaBoostClassifier
-}
 
 def timeit(func):
     @wraps(func)
@@ -78,21 +62,6 @@ def filter_test_data(data, max_length=25):
     return data
 
 
-def load_base_model_and_tokenizer(name, cache_dir):
-
-    base_model = transformers.AutoModelForCausalLM.from_pretrained(
-        name, cache_dir=cache_dir)
-    base_tokenizer = transformers.AutoTokenizer.from_pretrained(
-        name, cache_dir=cache_dir)
-    base_tokenizer.pad_token_id = base_tokenizer.eos_token_id
-    if base_tokenizer.pad_token is None:
-        base_tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-        base_model.resize_token_embeddings(len(base_tokenizer))
-
-
-    return base_model, base_tokenizer
-
-
 def move_model_to_device(model, DEVICE):
     DEFAULT_DEVICE = "cpu"
     
@@ -143,34 +112,6 @@ def cal_metrics(label, pred_label, pred_posteriors):
 
     return acc, precision, recall, f1, auc
 
-
-def get_clf_results(x_train, y_train, x_test, y_test, config):
-
-    clf_algo_config = config["clf_algo_for_threshold"]
-    clf_algo_name = clf_algo_config["name"]
-    if CLF_MODELS.get(clf_algo_name) is None:
-        raise ValueError(f"Unsupported classification algorithm for threshold computation selected: {clf_algo_name}")
-    
-    clf_algo = CLF_MODELS[clf_algo_name]
-    clf_model = clf_algo(**{key: value for key, value in clf_algo_config.items() if key != 'name'})
-    clf = clf_model.fit(x_train, y_train)
-
-    y_train_pred = clf.predict(x_train)
-    y_train_pred_prob = clf.predict_proba(x_train)
-    y_train_pred_prob = [_[1] for _ in y_train_pred_prob]
-    acc_train, precision_train, recall_train, f1_train, auc_train = cal_metrics(
-        y_train, y_train_pred, y_train_pred_prob)
-    train_res = acc_train, precision_train, recall_train, f1_train, auc_train
-
-    y_test_pred = clf.predict(x_test)
-    y_test_pred_prob = clf.predict_proba(x_test)
-    
-    y_test_pred_prob = [_[1] for _ in y_test_pred_prob]
-    acc_test, precision_test, recall_test, f1_test, auc_test = cal_metrics(
-        y_test, y_test_pred, y_test_pred_prob)
-    test_res = acc_test, precision_test, recall_test, f1_test, auc_test
-
-    return y_train_pred, y_test_pred, y_train_pred_prob, y_test_pred_prob, train_res, test_res
 
 def get_ll(text, base_model, base_tokenizer, DEVICE):
     with torch.no_grad():
